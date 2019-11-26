1cb235db21cdc474c8268e783118b9bcaaa36d46
ahmaduliel <ahmadulilamri09@gmail.com>
2019-11-26 18:57:50 +0700

    Updates.

---
+++ main.ts
@@ -0,0 +1,20 @@
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `);
+/**
+ * Functions are mapped to blocks using various macros 
+ * in comments starting with % (e.g., //% block).
+ * The most important macro "block" specifies that a
+ * block should be generated for a **exported** function.
+ */
+//% color="#AA278D"
+namespace roboqu {
+    //% block
+    export function camlCaseTwo() {
+
+    }
+}
