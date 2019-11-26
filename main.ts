6aa61173505635fbbce9e997c6f7ee50b22015af
ahmaduliel <ahmadulilamri09@gmail.com>
2019-11-26 19:11:01 +0700

    Updates.

--- main.ts
+++ main.ts
@@ -11,7 +11,7 @@ basic.showLeds(`
  * The most important macro "block" specifies that a
  * block should be generated for a **exported** function.
  */
-//% color="#AA278D"
+//% color="#AA278D" block="Roboqu"
 namespace roboqu {
     //% block
     export function camlCaseTwo() {
